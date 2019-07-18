3df30275fbab62ce82817b835438730d81580983
MartinD4574 <dcm9595@gmail.com>
2019-07-18 07:57:45 -0500

    Updated all references to Zuul
    Changed zuul references in pom.xml
    Changed application name in yml
    Updated package structure and class name

--- src/main/java/com/revature/eurekaapp/CmSforceMicroEurekaApplication.java
+++ src/main/java/com/revature/eurekaapp/CmSforceMicroEurekaApplication.java
@@ -1,16 +1,15 @@
-package com.example.zuulapp;
+package com.revature.eurekaapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @EnableEurekaServer
-@EnableAutoConfiguration
 @SpringBootApplication
-public class CmSforceMicroZuulApplication {
+public class CmSforceMicroEurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CmSforceMicroZuulApplication.class, args);
+		SpringApplication.run(CmSforceMicroEurekaApplication.class, args);
 	}
 
 }
